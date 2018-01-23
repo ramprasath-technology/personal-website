b972e02729cdcd37f783068792dbc4d9303dcde9
Ramprasath Babu <ramprasath.technology@gmail.com>
2018-01-23 01:16:40 -0600

    skills, testimonies and certifications

--- scripts/about.js
+++ scripts/about.js
@@ -1,7 +1,9 @@
-var welcomeMessage = "Welcome! Glad to see you here. I'm Ram and this page is about me. Scroll down to know more.";
+var welcomeMessage = "Welcome! I'm Ram and I'm glad to host host you here. Play around to know more about me. Scroll down.";
 var textToShow = "";
 var counter = 0;  
+var certPointer = 1;
 var interval; 
+var certInterval;
 var gradients = ["linear-gradient(to bottom left, #33ccff 0%, #ff99cc 100%)",
                  "linear-gradient(to bottom right, #33ccff 0%, #ff99cc 100%)",
                  "linear-gradient(to top right, #33ccff 0%, #ff99cc 100%)",
@@ -16,7 +18,28 @@ window.onload = function(){
 }
 
 function initialize(){ 
-    interval = setInterval(setText,300);
+    interval = setInterval(setText,100);
+    certInterval = setInterval(setCertColor, 1000);
+}
+
+function setCertColor(){
+    var prevPointer;
+
+    if(certPointer === 1)
+        prevPointer = 5;
+     else 
+        prevPointer = certPointer - 1;   
+
+    var prevElement = "cert_" + prevPointer;
+    document.getElementById(prevElement).style.backgroundColor = "";
+
+    var certElement = "cert_" + certPointer;
+    document.getElementById(certElement).style.backgroundColor = "#7FFFD4";
+
+    if(certPointer === 5)
+        certPointer = 1;
+    else    
+        certPointer++;   
 }
 
 function setText(){
